d94ad53653991138e661067a056ef18a036ae8a0
emre <emre@emrexps>
2018-01-16 10:30:11 +0300

    Message added

--- src/main/java/com/springsample/mvc/MvcApplication.java
+++ src/main/java/com/springsample/mvc/MvcApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MvcApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello from mvc");
 		SpringApplication.run(MvcApplication.class, args);
+		
 	}
 }
